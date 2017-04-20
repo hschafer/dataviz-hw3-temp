7c1a3187a1a7932d33181f3675e448a736e354c8
Meredith Lampe <mlampe@cs.washington.edu>
2017-04-20 11:35:59 -0700

    example d3 for csv

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
+
+    // here's how we get our csv files in d3
+    d3.csv("data/thecounted-data/the-counted-2015.csv", function(data) {
+          console.log(data[0]);
+    });
 });
