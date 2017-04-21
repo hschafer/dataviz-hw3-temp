5056700acab030fb24b12af6bf4cd99c7876f3eb
Hunter Schafer <hschafer@cs.washington.edu>
2017-04-21 10:35:47 -0700

    Some minor changes to UI

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        autoScrolling: false,
+        navigation: true,
+        navigationPosition: 'left',
+        menu: '#menu'
+
+    });
 
     // here's how we get our csv files in d3
     d3.csv("data/thecounted-data/the-counted-2015.csv", function(data) {
